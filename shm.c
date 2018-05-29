4a94ee772a9790b3ba2a344b25f06eda56a1e32b
Brett McCausland <bmcca009@sledge.cs.ucr.edu>
2018-05-29 11:46:45 -0700

    psudo code

--- shm.c
+++ shm.c
@@ -28,22 +28,49 @@ void shminit() {
   release(&(shm_table.lock));
 }
 
-int shm_open(int id, char **pointer) {
+int shm_open(int id, char **pointer)
+{
+  
+  acquire(&(shm_table.lock));                                                             
+  for (i = 0; i< 64; i++) 
+  { 
+    if(shm_table.shm_pages[i].id==id)//if it finds id in table
+    {  // find the physical address of the page in the table, and map it to an available page 
+       //uses mappages to add the mapping	
+       shm_table.shm_pages[i].refcnt+=1; 
+       //pointer=virtual adress
+       //update sz sice virt adress space expanded
+       release(&(shm_table.lock));
+       return 0;
+    }                                                              
+  }
+  //case it doesnt find the id in shm_table
+   //find an empty entry in shm_table
+   shm_table.shm_pages[pos].id=id; //initialize its id to the id passed in
+   //kmalloc a page and store its address in frame
+   shm_table.shm_pages[pos].refcnt=1;//set refcnt to 1
+   //mappage to avaliable VA space page
+   //store page mapping in shm_table
 
-//you write this
-
-
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
-}
-
-
-int shm_close(int id) {
-//you write this too!
-
-
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
+   //pointer=virtual adress
+   //update sz sice virt adress space expanded 
+  release(&(shm_table.lock));
+  return 0;
+}                                                                                        
+int shm_close(int id)
+{
+  //you write this too!
+  acquire(&(shm_table.lock));
+  for (i = 0; i< 64; i++)
+  {
+    if(shm_table.shm_pages[i].id==id)//if it finds id in table
+    {
+       shm_table.shm_pages[i].refcnt-=1;
+       //uses mappages to add the mapping 
+       //pointer=virtual adress
+       release(&(shm_table.lock));
+      return 0;
+    }
+  } 
+ return 0; //added to remove compiler warning -- you should decide what to return
 }
