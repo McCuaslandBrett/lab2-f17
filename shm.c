c349c2b9100f7a0de38f5ee062f617ee106c3fff
brettmccausland <brettmccausland@yahoo.com>
2018-06-02 12:04:28 -0700

    s

--- shm.c
+++ shm.c
@@ -48,7 +48,7 @@ int shm_open(int id, char **pointer)
       mappages(myproc()->pgdir, (void*) PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
       shm_table.shm_pages[i].refcnt++;
       //pointer=virtual adress
-      *pointer=(char *) PGROUNDUP(myproc()->sz);
+      *pointer=((char *) PGROUNDUP(myproc()->sz));
       //*pointer = (char*)shm_table.shm_pages[i].frame;
       release(&(shm_table.lock));
       return 0;
